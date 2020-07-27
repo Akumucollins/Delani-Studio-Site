9facd464e012d4639be5c8e5db801b71491ff87b
Akumu collins <akumucollins001@gmail.com>
2020-07-27 22:12:54 +0300

    created a mailchimp in the form input and added a function and change the button padding and width

--- js/script.js
+++ js/script.js
@@ -10,37 +10,15 @@ $(document).ready(function () {
   });
 });
 
-$(document).ready(function () {
-  $("#formInput").submit(function (event) {
-    var name = "input#name".val();
-    console.log(name);
-  });
-  $("#formInput").submit(function (event) {
+$(document).ready(function() {
+  $("form#formValidation").submit(function() {
+    var name = $("input#name").val();
     var email = $("input#email").val();
-    console.log(email);
-  });
-  $("#formInput").submit(function (event) {
     var message = $("textarea#message").val();
-    console.log(message);
+    if(name && email){
+      alert(  " Dear " + name + " we have warmly received your message. " + " Thank you for reaching out to us " + ". We are looking forward to replay to you soon.");
+    } else{
+      alert(" The field form is Incomplete.");
+    }
   });
-  $("button").on("click", function () {
-    $("form").each(function () {
-      this.reset();
-    });
-  });
-
-  event.preventDefault();
 });
-
-$("button").click(function (event) {
-  var customer = document.getElementById("name").value;
-  alert(
-    " Dear " +
-      customer +
-      " we have warmly received your message. " +
-      " Thank you for reaching out to us " +
-      " We are looking forward to replay to you soon."
-  );
-  event.preventDefault();
-});
-
